2a23b80ad6ee3337dc420c9c689061e12f3862e3
Antorweep Chakravorty <chakravrty.antorweep@gmail.com>
2020-01-28 15:19:51 +0100

    added few hanges

--- roles/hlf/peer/files/peer.sh
+++ roles/hlf/peer/files/peer.sh
@@ -1,12 +1,12 @@
-#mkdir -p $CORE_PEER_MSPCONFIGPATH/admincerts
-#cp $CORE_PEER_MSPCONFIGPATH/signcerts/* $CORE_PEER_MSPCONFIGPATH/admincerts/
-
 #Start the peer
 peer node start &
 
-# Hotfix. Later check if peer has started before rtunning the following commands
-sleep 15s
+while ! nc -z $CORE_PEER_ID 7051; do   
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
 # Join the peers to the application channel
-# Peer1
 CORE_PEER_TLS_ROOTCERT_FILE=/root/${CORE_PEER_ID}/tls-msp/tlscacerts/tls-${TLSCA_HOST}-7054.pem
+CORE_PEER_MSPCONFIGPATH=/root/admin/msp
+
 CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH CORE_PEER_ADDRESS=$CORE_PEER_ID:7051 CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE peer channel join -b /root/peer_cli/artifacts/appchannel.block
