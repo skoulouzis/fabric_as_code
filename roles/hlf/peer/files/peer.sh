d2d3ee7a9a44c6aaf350b07314406ee791a97ac3
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-01-28 14:35:25 +0100

    - peer restart bug fix changes

--- roles/hlf/peer/files/peer.sh
+++ roles/hlf/peer/files/peer.sh
@@ -1,10 +1,12 @@
-CORE_PEER_MSPCONFIGPATH=/root/admin/msp 
-mkdir -p $CORE_PEER_MSPCONFIGPATH/admincerts
-cp $CORE_PEER_MSPCONFIGPATH/signcerts/* $CORE_PEER_MSPCONFIGPATH/admincerts/
+#mkdir -p $CORE_PEER_MSPCONFIGPATH/admincerts
+#cp $CORE_PEER_MSPCONFIGPATH/signcerts/* $CORE_PEER_MSPCONFIGPATH/admincerts/
 
+#Start the peer
+peer node start &
+
+# Hotfix. Later check if peer has started before rtunning the following commands
+sleep 15s
 # Join the peers to the application channel
 # Peer1
 CORE_PEER_TLS_ROOTCERT_FILE=/root/${CORE_PEER_ID}/tls-msp/tlscacerts/tls-${TLSCA_HOST}-7054.pem
 CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH CORE_PEER_ADDRESS=$CORE_PEER_ID:7051 CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE peer channel join -b /root/peer_cli/artifacts/appchannel.block
-
-peer node start
